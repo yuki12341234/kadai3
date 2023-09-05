944d0a7b50bc98dcb2ae63d3ada9a6f853f769fb
EC2 Default User <ec2-user@ip-172-31-46-238.ap-northeast-1.compute.internal>
2023-09-05 09:45:03 +0000

    kadai4[finish]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   
   has_one_attached :profile_image
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   def get_profile_image
     unless profile_image.attached?
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about', to: 'homes#about', as: 'about'
-  resources :books, only: [:new, :index, :show, :edit, :update, :create, :destroy]
+  resources :books, only: [:index, :show, :edit, :update, :create, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   resources :users, only:[:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
